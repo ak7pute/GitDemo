4b7f4a361492c9da0619f62080598be02cfebefa
hp <hp@om>
2020-12-30 14:42:19 +0530

    this is second commit

---
+++ GitDemo/src/com/cjc/BasicDemo.java
@@ -0,0 +1,19 @@
+package com.cjc;
+
+public class BasicDemo 
+{
+	int i=10;
+	String name="akshay";
+	
+	public void Display()
+	{
+		System.out.println(i);
+		System.out.println(name);
+	}
+	public static void main(String[] args) {
+		
+		BasicDemo bd=new BasicDemo();
+		bd.Display();
+	}
+
+}
